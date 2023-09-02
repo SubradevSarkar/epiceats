1d6caa674317b451329cfb03df9e154de383157b
subradev sarkar <subradevssarkar@gmail.com>
2023-09-03 03:00:42 +0530

    update - middleware

--- app.js
+++ app.js
@@ -4,6 +4,11 @@ const expressLayout = require("express-ejs-layouts");
 const errorHandler = require("./server/middleware/errorMiddleware");
 const connectDB = require("./server/config/dbconfig");
 
+const fileUpload = require("express-fileupload");
+const session = require("express-session");
+const cookieParser = require("cookie-parser");
+const flash = require("connect-flash");
+
 const app = express();
 const port = process.env.PORT || 5000;
 connectDB();
@@ -12,6 +17,17 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.static("public"));
 app.use(expressLayout);
 app.use(errorHandler);
+
+app.use(cookieParser("CookingBlogSecure"));
+app.use(
+  session({
+    secret: "CookingBlogSecretSession",
+    saveUninitialized: true,
+    resave: true,
+  })
+);
+app.use(flash());
+app.use(fileUpload());
 
 app.set("layout", "./layouts/main");
 app.set("view engine", "ejs");
