5c29344ce92a5875852a93a33824dc3c6f34a936
subradev sarkar <subradevssarkar@gmail.com>
2023-09-20 20:32:51 +0530

    fix - authorization functionality

--- server/middleware/authMiddleware.js
+++ server/middleware/authMiddleware.js
@@ -3,7 +3,6 @@ import { verifyToken } from "../utils/manageToken.js";
 
 const authUser = async (req, res, next) => {
   let token = req.cookies.jwt;
-
   if (token) {
     try {
       const decode = await verifyToken(token);
@@ -23,13 +22,13 @@ const authUser = async (req, res, next) => {
 const hasAccess = ([...roles]) => {
   return (req, res, next) => {
     if (!req.user) {
-      res.redirect("/user/login");
+      return res.redirect("/user/login");
     }
 
     if (req.user.isAdmin || roles.includes(req.user.role)) {
-      next();
+      return next();
     } else {
-      res.redirect("/");
+      return res.redirect("/");
     }
   };
 };
